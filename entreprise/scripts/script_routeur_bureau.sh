b630a45793ead073fc3d0ba6d1f7f44bd50a3266
Athos-0day <athos.0day@protonmail.com>
2025-11-25 20:55:55 +0100

    add: les routes des deux routeurs

--- entreprise/scripts/script_routeur_bureau.sh
+++ entreprise/scripts/script_routeur_bureau.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 # Script pour la gestion des interfaces de la box
+echo "[INFO] Configuration du routeur bureau"
+# Interface
 
-# Creation d'un bridge entre eth1 et eth2
+# Côté machines
 ip link add name br0 type bridge
 ip link set eth1 master br0
 ip link set eth2 master br0
 ip link set eth1 up
 ip link set eth2 up
 ip link set br0 up
-ip link set dev eth0 up
-# adresse ip locale
 ip addr add 192.168.49.49/24 dev br0
-ip addr add 192.168.49.34/24 dev eth0
+
+# Côté routeur
+ip link add name br1 type bridge
+ip link set eth3 master br1
+ip link set eth4 master br1
+ip link set dev eth3 up
+ip link set dev eth4 up
+ip link set br1 up
+ip addr add 192.168.49.34/24 dev br1
+
+# Routage
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Ajout des routes
+ip route add 192.168.49.16/28 via 192.168.49.2  # vers réseau services
+ip route add default via 192.168.49.1            # sortie vers le routeur public
 
 # On démarre le service DHCP
 /etc/init.d/isc-dhcp-server start > /dev/null &
+
+echo "[INFO] Routeur bureau configuré."
 
